61075da8ea2872c8ca29db5ef99e5b365646e8a8
raellrrm <gamingprodf@gmail.com>
2025-10-19 15:43:38 -0300

    refactor: disable real CPF validation in filters

--- src/pages/users/filter-users-form.tsx
+++ src/pages/users/filter-users-form.tsx
@@ -9,11 +9,15 @@ import { cpf } from "cpf-cnpj-validator";
 import { useSearchParams } from "react-router";
 
 const usersFilterSchema = z.object({
+    // cpf: z
+    //     .string()
+    //     .transform((val) => (val.trim() === "" ? undefined : val))
+    //     .optional()
+    //     .refine((value) => !value || cpf.isValid(value), { message: "CPF inválido." }),
     cpf: z
         .string()
-        .transform((val) => (val.trim() === "" ? undefined : val))
-        .optional()
-        .refine((value) => !value || cpf.isValid(value), { message: "CPF inválido." }),
+        .transform((val) => (val ?? "").replace(/\D/g, ""))
+        .refine((digits) => digits.length === 11, { message: "CPF inválido." }),
     status: z.string().optional()
 });
 
